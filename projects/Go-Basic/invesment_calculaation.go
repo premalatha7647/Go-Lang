6c7aa859ad07146a781eb9b955104cf462229e23
premalatha <169128822+premalatha7647@users.noreply.github.com>
2025-03-25 10:40:57 +0530

    revenue project add

--- projects/Go-Basic/invesment_calculaation.go
+++ projects/Go-Basic/invesment_calculaation.go
@@ -6,10 +6,18 @@ import (
 )
 
 func main() {
-	var invesment = 1000
-	var expectedReturn = 5.5
-	var years = 10
-
-	var futureValue = float64(invesment) * math.Pow((1+expectedReturn/100), float64(years))
-	fmt.Print(futureValue)
+	const inflationRate = 6.5
+	var investment float64
+	var years float64
+	var expectedReturn float64
+	fmt.Print("Investment Amount: ")
+	fmt.Scan(&investment)
+	fmt.Print("Expected Return Rate: ")
+	fmt.Scan(&expectedReturn)
+	fmt.Print("Years: ")
+	fmt.Scan(&years)
+	futureValue := investment * math.Pow((1+expectedReturn/100), years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+	fmt.Println(futureValue)
+	fmt.Println((futureRealValue))
 }
