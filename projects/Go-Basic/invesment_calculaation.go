e591feea3a2d4e645e1a83276761abd159a5c930
premalatha <169128822+premalatha7647@users.noreply.github.com>
2025-03-25 13:05:18 +0530

    functions

--- projects/Go-Basic/invesment_calculaation.go
+++ projects/Go-Basic/invesment_calculaation.go
@@ -5,19 +5,37 @@ import (
 	"math"
 )
 
+const inflationRate = 6.5
+
 func main() {
-	const inflationRate = 6.5
 	var investment float64
 	var years float64
-	var expectedReturn float64
+	expectedReturn := 2.5
 	fmt.Print("Investment Amount: ")
 	fmt.Scan(&investment)
-	fmt.Print("Expected Return Rate: ")
-	fmt.Scan(&expectedReturn)
+	// fmt.Print("Expected Return Rate: ")
+	// fmt.Scan(&expectedReturn)
 	fmt.Print("Years: ")
 	fmt.Scan(&years)
-	futureValue := investment * math.Pow((1+expectedReturn/100), years)
-	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
-	fmt.Println(futureValue)
-	fmt.Println((futureRealValue))
+
+	futureValue, futureRealValue := investmentCalc(investment, expectedReturn, years)
+
+	// futureValue := investment * math.Pow((1+expectedReturn/100), years)
+	// futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+
+	formatedFV := fmt.Sprintf("Future value: %.1f\n", futureValue)
+	formatedRFV := fmt.Sprintf("Future Real value: %.1f\n", futureRealValue)
+	fmt.Print(formatedFV, formatedRFV)
+
+	// fmt.Println("Future value", futureValue)
+	// fmt.Println((futureRealValue))
+	// fmt.Printf("Future value: %.3f\nFuture Real value: %v\n", futureValue, futureRealValue)
+	// fmt.Printf(`Future value: %.3f
+	// Future Real value: %v`, futureValue, futureRealValue)
+}
+
+func investmentCalc(investment, expectedReturn, years float64) (float64, float64) {
+	fv := investment * math.Pow((1+expectedReturn/100), years)
+	frv := fv / math.Pow(1+inflationRate/100, years)
+	return fv, frv
 }
